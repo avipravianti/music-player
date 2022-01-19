3ba46cd1761b65d822919ff2b419d78a2fd43375
Avi Pravianti <praviantiavi@gmail.com>
2022-01-19 21:22:44 +0700

    add: components, styles, data

---
+++ src/components/Player.js
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faPlay, faStepForward, faStepBackward } from '@fortawesome/free-solid-svg-icons'
+
+
+const Player = () => {
+    return (
+        <div className="player">
+            <div className="time-control">
+                <p>Start Time</p>
+                <input type="range"/>
+                <p>End Time</p>
+            </div>
+            <div className="play-control">
+                <FontAwesomeIcon className='previous' size="2x" icon={faStepBackward}/>
+                <FontAwesomeIcon className='play' size="2x" icon={faPlay}/>
+                <FontAwesomeIcon className='next' size="2x" icon={faStepForward}/>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Player 
